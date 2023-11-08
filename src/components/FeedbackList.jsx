e461ebc5fd0066111d3a136769c8b212b3e43b44
Anderson Martins <atmmoreira.rj@gmail.com>
2023-11-07 23:04:01 -0300

    update: prop-type shape FeedbackList

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -12,7 +12,13 @@ function FeedbackList({ feedback }) {
 }
 
 FeedbackList.propTypes = {
-  feedback: PropTypes.array.isRequired,
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
 };
 
 export default FeedbackList;
