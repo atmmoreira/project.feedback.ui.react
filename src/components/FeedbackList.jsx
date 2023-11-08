ba1e63e78bc4a7eb2279c5e3c019c97e2dda400c
Anderson Martins <atmmoreira.rj@gmail.com>
2023-11-07 22:54:02 -0300

    fix: prop-types in components

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,11 +1,18 @@
-import FeedbackItem from './FeedbackItem'
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedback }) {
-    return (
-        <div className="feedback-list">
-            {feedback.map(item => (<FeedbackItem key={item.id} item={item} />))}
-        </div>
-    )
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
 }
-export default FeedbackList;
 
+FeedbackList.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
+
+export default FeedbackList;
