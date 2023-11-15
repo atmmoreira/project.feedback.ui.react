519df8da48fcd0cb28503a1bd4ea6b35d0967d4f
Anderson Martins <atmmoreira.rj@gmail.com>
2023-11-14 21:08:35 -0300

    feature: add uuid, addFeedback function to render list of feed

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,14 +3,14 @@ import Card from '../shared/Card'
 import Button from '../shared/Button'
 import RatingSelect from './RatingSelect';
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
     const [text, setText] = useState('');
     const [rating, setRating] = useState(10);
     const [btnDisable, setBtnDisable] = useState(true);
     const [message, setMessage] = useState('');
 
     const handleTextChange = (e) => {
-        if(text === ''){
+        if (text === '') {
             setBtnDisable(true)
             setMessage(null)
         } else if (text !== '' && text.trim().length <= 10) {
@@ -24,10 +24,21 @@ function FeedbackForm() {
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text, rating
+            }
+            handleAdd(newFeedback);
+            setText('');
+        }
+    }
+
     return <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>How would you rate your service with us?</h2>
-            <RatingSelect select={(rating) => setRating(rating)}/>
+            <RatingSelect select={(rating) => setRating(rating)} />
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
                 <Button type='submit' isDisable={btnDisable}>Send</Button>
