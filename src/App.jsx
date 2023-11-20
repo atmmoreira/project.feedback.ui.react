2cdc0d73c78a0a08d5c9917f3387ef5a367a24f7
Anderson Martins <atmmoreira.rj@gmail.com>
2023-11-20 19:15:11 -0300

    refactor: add a CRUD, and implementing a json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
-
-import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackList";
-import FeedbackStats from "./components/FeedbackStats";
-import FeedbackForm from "./components/FeedbackForm";
-import { FeedbackProvider } from "./context/FeedbackContext";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-
   return (
-    <>
-      <FeedbackProvider>
+    <FeedbackProvider>
+      <Router>
         <Header />
-        <div className="container">
-          <FeedbackForm />
-          <FeedbackStats />
-          <FeedbackList />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
         </div>
-      </FeedbackProvider>
-    </>
-  );
+      </Router>
+    </FeedbackProvider>
+  )
 }
 
-export default App;
+export default App
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,23 +1,20 @@
-import { useContext } from "react";
-import FeedbackContext from "../context/FeedbackContext";
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackStats() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback } = useContext(FeedbackContext)
 
-  // Calculation rating average
-  let average =
-    feedback.reduce((acc, curr) => {
-      return acc + curr.rating;
-    }, 0) / feedback.length;
-
-  average = average.toFixed(1).replace(/[.,]0$/, "");
+  const average =
+    feedback.length === 0
+      ? 0
+      : feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length
 
   return (
-    <div className="feedback-stats">
-      <h4>{feedback.length} reviews</h4>
-      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    <div className='feedback-stats'>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {average.toFixed(1).replace(/[.,]0$/, '')}</h4>
     </div>
-  );
+  )
 }
 
-export default FeedbackStats;
+export default FeedbackStats
